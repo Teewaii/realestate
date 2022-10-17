35f565b54eecde69cf12e71e481383c473aa0fae
Taiye Ajimati <taiyeajimati@gmail.com>
2022-10-17 17:41:57 +0400

    last office commit

--- portfolio/portfolio/src/components/nav/Nav.jsx
+++ portfolio/portfolio/src/components/nav/Nav.jsx
@@ -14,22 +14,22 @@ export default function Nav({ toggle, toggleMenu}) {
         <div className="links">
         <ul className={toggle?'hidden absolute md:relative container bg-white  top-0 left-0 right-0 z-[-2] pt-[128px] pb-[44px]  md:pt-0 md:pb-0  flex-col md:flex-row items-start gap-[20px] md:gap-[27px] mb-[30px] md:mb-0':'flex absolute md:relative container  top-0 left-0 right-0 z-[-2] pt-[128px] pb-[44px]  md:pt-0 md:pb-0  flex-col md:flex-row items-start gap-[20px] md:gap-[27px] mb-[30px] md:mb-0'}>
                 
-                <li className='border-b-[0.2px] border-opacity-[0.3] border-lightprim pb-[20px] w-[100%] text-left hover:border-b-primary hover:w-[80%] ease-in-out duration-300 
+                <li className='border-b-[1px] border-opacity-[0.3] border-lightprim pb-[20px] w-[100%] text-left hover:border-b-primary hover:w-[80%] ease-in-out duration-300 
                 md:border-b-0 md:pb-0
                 '><a className='text-[17px] text-primary font-medium ml-[20px] 
                 md:ml-0
                 ' href="#">About</a></li>
-                <li className='border-b-[0.2px] border-opacity-[0.3] border-lightprim pb-[20px] w-[100%] text-left hover:border-b-primary hover:w-[80%] ease-in-out duration-300 
+                <li className='border-b-[1px] border-opacity-[0.3] border-lightprim pb-[20px] w-[100%] text-left hover:border-b-primary hover:w-[80%] ease-in-out duration-300 
                 md:border-b-0 md:pb-0
                 '><a className='text-[17px] text-primary font-medium ml-[20px] 
                 md:ml-0
                 ' href="#">Experience</a></li>
-                <li className='border-b-[0.2px] border-opacity-[0.3] border-lightprim pb-[20px] w-[100%] text-left hover:border-b-primary hover:w-[80%] ease-in-out duration-300 
+                <li className='border-b-[1px] border-opacity-[0.3] border-lightprim pb-[20px] w-[100%] text-left hover:border-b-primary hover:w-[80%] ease-in-out duration-300 
                 md:border-b-0 md:pb-0
                 '><a className='text-[17px] text-primary font-medium ml-[20px] 
                 md:ml-0
                 ' href="#">Projects</a></li>
-                <li className='border-b-[0.2px] border-opacity-[0.3] border-lightprim pb-[20px] w-[100%] text-left hover:border-b-primary hover:w-[80%] ease-in-out duration-300 
+                <li className='border-b-[1px] border-opacity-[0.3] border-lightprim pb-[20px] w-[100%] text-left hover:border-b-primary hover:w-[80%] ease-in-out duration-300 
                 md:border-b-0 md:pb-0
                 '><a className='text-[17px] text-primary font-medium ml-[20px] 
                 md:ml-0
