d235a31479909e0b0799414e6ed3c41ae67e4537
Taiye <taiyeajimati@gmail.com>
2022-10-19 00:03:06 +0400

    Last home commit

--- portfolio/portfolio/src/components/btn/Btn.jsx
+++ portfolio/portfolio/src/components/btn/Btn.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import right_arrow from '../../img/rightArrow.svg';
 export default function Btn() {
   return (
-    <div >
-        <button className='rounded-[10px] py-[12px] px-[35px] lg:py-[20px] lg:px-[50px] text-secondary text-[20px] font-medium ease-in-out duration-300 bg-primary hover:bg-secondary hover:text-primary'>Say hello!</button>
+    <div className='relative w-fit' >
+      <button className='flex flex-row-reverse gap-2 md:gap-3 items-center rounded-[10px] py-[12px] px-[35px] lg:py-[20px] lg:px-[50px] text-secondary text-[20px] font-medium ease-in-out duration-300 bg-primary hover:bg-secondary hover:text-primary '><span className='animate-pulse'><img className='w-[20px] md:w-[25px]' src={right_arrow} alt="waht's next" /></span>Say hello!</button>
+      {/* <span className='absolute w-[20px] h-[20px] right-[-5px] rounded-full bg-secondary animate-bounce'></span> */}
     </div>
   )
 }
