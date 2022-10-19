b3faae9973cd36ee5ba1ef2a43d26e77262e0522
Taiye <taiyeajimati@gmail.com>
2022-10-20 00:03:06 +0400

    Last home commit

--- portfolio/portfolio/src/components/btn/Mode_mob.jsx
+++ portfolio/portfolio/src/components/btn/Mode_mob.jsx
@@ -4,13 +4,13 @@ import { SunIcon } from '@heroicons/react/24/solid'
 
 export default function Mode_mob({ dark_mode, DarkMode }) {
   return (
-    <div className='mode cursor-pointer tool  group absolute  top-[4.5%] left-[70%] S
+    <div className='mode cursor-pointer tool  group absolute  top-[6%] left-[70%] S
      md:relative md:hidden
       ' onClick={DarkMode}>
       <small className=' hidden absolute mt-[-20px] -ml-[20px] w-[100px] text-[13px]  group-hover:block  ease-in-out duration-300  font-bold text-lightprim'>{!dark_mode ? 'Night mode' : 'Day mode'}</small>
       {dark_mode ?
         <SunIcon className='w-[30px] md:w-[30px] fill-primary dark:fill-secondary animate-pulse' /> :
-        <MoonIcon className='w-[28px] md:w-[28px] fill-primary dark:fill-secondary animate-pulse' />}
+        <MoonIcon className='w-[26px] md:w-[26px] fill-primary dark:fill-secondary animate-pulse' />}
     </div>
   )
 }
