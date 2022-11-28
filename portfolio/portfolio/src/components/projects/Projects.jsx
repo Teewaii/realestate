ad21816609d8cd86e1f3cb62abccc87bb700bff6
Taiye Ajimati <taiyeajimati@gmail.com>
2022-11-28 18:56:06 +0400

    First office commit

--- portfolio/portfolio/src/components/projects/Projects.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Projects() {
-  return (
-    <section id='projects'>
-    <div className='projects bg-green-300 h-[95vh]'>Hello</div>
-    </section>
-  )
-}
